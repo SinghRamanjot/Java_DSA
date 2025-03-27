2acce31236b3ab72929d353c7f679153a2299408
Ramanjot Singh <singhramanjot65@gmail.com>
2025-03-27 21:15:34 +0530

    Recursive Binary Search

--- BinarySearch/BinarySearch.java
+++ BinarySearch/BinarySearch.java
@@ -2,7 +2,7 @@ public class BinarySearch {
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
         int target = 4;
-        int result = binarySearch(arr, target);
+        int result = recursiveBinarySearch(arr, target, 0, arr.length - 1);
         if (result == -1) {
             System.out.println("Element not found in the array.");
         } else {
@@ -29,5 +29,17 @@ public class BinarySearch {
         return -1;
     }
 
-    
+    public static int recursiveBinarySearch(int[] arr, int target, int left, int right) {
+        int mid = (left + right) / 2;
+
+        if (arr[mid] == target) {
+            return mid;
+        } else if (arr[mid] < target) {
+            return recursiveBinarySearch(arr, target, mid + 1, right);
+        } else if (arr[mid] > target) {
+            return recursiveBinarySearch(arr, target, left, mid - 1);
+        }
+
+        return recursiveBinarySearch(arr, target, 0, arr.length - 1);
+    }
 }
