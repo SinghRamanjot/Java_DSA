eb671a95941b380e165f46b39cde84f0af8c4dc5
Ramanjot Singh <singhramanjot65@gmail.com>
2025-03-29 21:13:48 +0530

    selection sort

---
+++ SelectionSort/SelectionSort.java
@@ -0,0 +1,25 @@
+public class SelectionSort {
+    public static void main(String[] args) {
+        int nums[] = { 64, 34, 25, 12, 22, 11, 90 };
+        int minIndex = -1;
+
+        int temp = 0;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+        minIndex = i;
+        for (int j = i + 1; j < nums.length; j++) {
+        if (nums[minIndex] > nums[j]) {
+        minIndex = j;
+        }
+        }
+        temp = nums[minIndex];
+        nums[minIndex] = nums[i];
+        nums[i] = temp;
+
+        }
+
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+}
